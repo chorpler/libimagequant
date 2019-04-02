194eeb11f033fb9e44b52b5da3919559cb085d83
David Sargeant <chorpler@gmail.com>
2019-04-02 13:54:12 -0500

    Cmake version
    Ran cmake and built DLL

---
+++ CMakeFiles/imagequant.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/code/forks/libimagequant/blur.c" "/code/forks/libimagequant/CMakeFiles/imagequant.dir/blur.c.obj"
+  "/code/forks/libimagequant/kmeans.c" "/code/forks/libimagequant/CMakeFiles/imagequant.dir/kmeans.c.obj"
+  "/code/forks/libimagequant/libimagequant.c" "/code/forks/libimagequant/CMakeFiles/imagequant.dir/libimagequant.c.obj"
+  "/code/forks/libimagequant/mediancut.c" "/code/forks/libimagequant/CMakeFiles/imagequant.dir/mediancut.c.obj"
+  "/code/forks/libimagequant/mempool.c" "/code/forks/libimagequant/CMakeFiles/imagequant.dir/mempool.c.obj"
+  "/code/forks/libimagequant/nearest.c" "/code/forks/libimagequant/CMakeFiles/imagequant.dir/nearest.c.obj"
+  "/code/forks/libimagequant/pam.c" "/code/forks/libimagequant/CMakeFiles/imagequant.dir/pam.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "USE_SSE=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/imagequant.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imagequant.dir/libimagequant.c.obj"
+  "CMakeFiles/imagequant.dir/blur.c.obj"
+  "CMakeFiles/imagequant.dir/mediancut.c.obj"
+  "CMakeFiles/imagequant.dir/mempool.c.obj"
+  "CMakeFiles/imagequant.dir/nearest.c.obj"
+  "CMakeFiles/imagequant.dir/pam.c.obj"
+  "CMakeFiles/imagequant.dir/kmeans.c.obj"
+  "libimagequant.pdb"
+  "libimagequant.dll"
+  "libimagequant.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/imagequant.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
